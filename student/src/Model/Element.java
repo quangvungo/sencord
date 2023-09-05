e1346730fb87abae5378ea7219c3a0ea9d351f07
quangvungo <133740722+quangvungo@users.noreply.github.com>
2023-09-05 22:18:31 +0700

    Add files via upload

---
+++ student/src/Model/Element.java
@@ -0,0 +1,18 @@
+package Model;
+
+public class Element {
+    public int size_array;
+    public int[] array;
+    public void setSize_array(int size_array) {
+        this.size_array = size_array;
+    }
+    public void setArray(int[] array) {
+        this.array = array;
+    }
+    public int getSize_array() {
+        return size_array;
+    }
+    public int[] getArray() {
+        return array;
+    }
+}
